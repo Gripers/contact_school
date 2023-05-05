6714c6a65117e189d4929d6351132a16549802ca
gripers <gripers91@gmail.com>
2023-05-05 16:28:41 +0500

    fst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,55 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import styles from './page.module.css';
+
+import { Toaster, toast } from 'react-hot-toast';
+import Image from 'next/image';
+import axios from 'axios';
 
 export default function Home() {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    await axios
+      .post(
+        `https://api.telegram.org/bot${
+          process.env.NEXT_PUBLIC_BOT
+        }/sendMessage?chat_id=${
+          process.env.NEXT_PUBLIC_GROUP
+        }&text=${encodeURIComponent(
+          `<b>Name, Surname:</b> ${event.target[0].value}
+<b>Age:</b> ${event.target[1].value}
+<b>Phone number:</b> ${event.target[2].value}
+        `
+        )}&parse_mode=html`
+      )
+      .then(() => {
+        event.target.reset();
+        toast.success('Success');
+      })
+      .catch(() => toast.error('Fail'));
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className={styles.main}>
+        <div className={styles.wrapper}>
+          <Image
+            src='/static/images/logo.png'
+            alt=''
+            width={150}
+            height={150}
+          />
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <input type='text' placeholder='Name, Surname' required />
+            <input type='number' placeholder='Age' required />
+            <input type='number' placeholder='Phone number' required />
+            <button type='submit'>Submit</button>
+          </form>
         </div>
-      </div>
+      </main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Toaster position='top-center' />
+    </>
+  );
 }
